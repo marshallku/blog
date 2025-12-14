c6dafb02c5c5cec143c74179f3d052e019a324b2
Marshall Ku <marshall@kakao.com>
2025-12-14 15:09:49 +0900

    Fix to copy templates in container

--- crates/backend/src/templates.rs
+++ crates/backend/src/templates.rs
@@ -1,8 +1,10 @@
-use std::sync::LazyLock;
+use std::{env, sync::LazyLock};
 use tera::Tera;
 
 pub static TEMPLATES: LazyLock<Tera> = LazyLock::new(|| {
-    let mut tera = match Tera::new("crates/backend/templates/**/*.html") {
+    let template_path = env::var("TEMPLATE_PATH")
+        .unwrap_or_else(|_| "crates/backend/templates/**/*.html".to_string());
+    let mut tera = match Tera::new(&template_path) {
         Ok(t) => t,
         Err(e) => {
             log::error!("Template parsing error: {}", e);
